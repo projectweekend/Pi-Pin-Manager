1ef72e4c170292981a45a5124ca1f88ada95f770
Brian Hines <brian@projectweekend.net>
2014-12-24 12:15:16 -0500

    Moving around test stuff

---
+++ tests.py
@@ -0,0 +1,13 @@
+import unittest
+import mock
+
+from pins import PinManager
+from testing.mock_gpio import MockGPIO
+
+
+
+@mock.patch('RPi.GPIO', MockGPIO)
+class PinManagerTests(unittest.TestCase):
+
+	def test_pin_manager(self):
+		manager = PinManager()
