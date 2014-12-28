91eb4582da21041ec894f6ffc5c1126ab8612a8e
Brian Hines <brian@projectweekend.net>
2014-12-27 20:21:20 -0600

    Pip this

---
+++ setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+setup(
+    name='Pi-Pin-Manager',
+    version='0.0.1',
+    author='Brian Hines',
+    author_email='brian@projectweekend.net',
+    packages=['pi_pin_manager'],
+    url='http://pypi.python.org/pypi/Pi-Pin-Manager/',
+    license='LICENSE.txt',
+    description='Manage RPi.GPIO pin definition and initialization with a config file.',
+    long_description=open('README.md').read(),
+    install_requires=[
+        "PyYAML == 3.11",
+        "RPi.GPIO == 0.5.8",
+    ],
+)
