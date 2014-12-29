986bad2c2f56ca989f8d2b7e75730ba6ce10672c
Brian Hines <brian@projectweekend.net>
2014-12-29 07:35:30 -0600

    Making a new release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='Pi-Pin-Manager',
-    version='0.0.2',
+    version='0.0.3',
     author='Brian Hines',
     author_email='brian@projectweekend.net',
     packages=['pi_pin_manager'],
