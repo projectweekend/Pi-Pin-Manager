acf60c7fada6a1ba1886535157ad1a189f9fa20e
Brian Hines <brian@projectweekend.net>
2015-02-28 08:45:21 -0600

    Multiple pin watcher

--- pi_pin_manager/pins.py
+++ pi_pin_manager/pins.py
@@ -50,6 +50,37 @@ class SinglePinWatcher(GPIOConfig, GPIOActions):
             self.cleanup()
 
 
+class MultiplePinWatcher(GPIOConfig, GPIOActions):
+    """This class is used with multiple pins. Calling the 'start' method will
+    block, waiting for changes in state for the defined pins. When that happens
+    a method on the `event_handler` class, matching the 'handler' name from the
+    pin config is called. Each of these 'handler' methods takes a single argument,
+    the pin number, which is passed in automatically by the RPi.GPIO library.
+
+    The 'event_handler' class should have one parameter in its constructor. An
+    instance of GPIO will be passed in so you have access to it in each custom
+    'handler' method."""
+
+    def __init__(self, config, event_handler):
+        super(MultiplePinWatcher, self).__init__(config=config, event_handler=event_handler)
+        self._initialize_gpio()
+        self._initialize_pins()
+
+    def _initialize_pins(self):
+        for pin_num, pin_options in self._pin_config.items():
+            self._setup_pin(pin_num, pin_options)
+
+    def start(self):
+        try:
+            # keep this running for the threaded callbacks
+            while True:
+                sleep(0.250)
+        except KeyboardInterrupt:
+            pass
+        finally:
+            self.cleanup()
+
+
 class PinManager(object):
 
     def __init__(self, config_file=None, config_dict=None, event_handlers=None):
