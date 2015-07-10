88f931aea6a8d2b1c24a1aa6d915f9a2ab9b04a9
Brian Hines <brian@projectweekend.net>
2015-07-10 05:44:20 -0500

    Split actions out

--- pi_pin_manager/gpio_actions.py
+++ pi_pin_manager/gpio_actions.py
@@ -1,36 +1,4 @@
-import yaml
 import RPi.GPIO as GPIO
-from validation import ConfigValidator, CONFIG_SCHEMA
-from errors import PinNotDefinedError, InvalidConfigurationError
-
-
-class GPIOConfig(object):
-
-    def __init__(self, config, event_handler=None):
-        self._config = config
-        self._event_handler = event_handler
-        self._load_pin_config()
-
-    def _load_pin_config(self):
-        if isinstance(self._config, list):
-            self._pin_config = self._config
-        else:
-            with open(self._config) as file:
-                self._pin_config = yaml.safe_load(file)
-        v = ConfigValidator(CONFIG_SCHEMA)
-        for item in self._pin_config:
-            if not v.validate(item):
-                message = "Invalid configuration file/dictionary. Check 'errors' for detail."
-                raise InvalidConfigurationError(message, v.errors)
-
-    def get_config(self, pin_number=None):
-        if pin_number:
-            try:
-                return self._pin_config[pin_number].copy()
-            except KeyError:
-                message = "Pin {0} not defined in configuration".format(pin_number)
-                raise PinNotDefinedError(message)
-        return self._pin_config.copy()
 
 
 class GPIOActions(object):
