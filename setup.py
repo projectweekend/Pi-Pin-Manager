350b8eed0e7db8c7ec1d03d281c9f7ba4198783a
Brian Hines <brian@projectweekend.net>
2015-07-18 09:44:00 -0500

    Publish new version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='Pi-Pin-Manager',
-    version='1.1.1',
+    version='2.0.0',
     author='Brian Hines',
     author_email='brian@projectweekend.net',
     packages=['pi_pin_manager'],
